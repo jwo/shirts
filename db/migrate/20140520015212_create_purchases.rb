8c0c8aba836c3b7fdc86295e2746f55b5f31f8c1
Jesse Wolgamott <jesse@comalproductions.com>
2014-05-22 12:13:39 -0500

    Change to be integer from the start

--- db/migrate/20140520015212_create_purchases.rb
+++ db/migrate/20140520015212_create_purchases.rb
@@ -2,7 +2,7 @@ class CreatePurchases < ActiveRecord::Migration
   def change
     create_table :purchases do |t|
       t.references :shirt, index: true
-      t.string :price_in_cents
+      t.integer :price_in_cents
       t.string :stripe_transaction
 
       t.timestamps
--- db/migrate/20140520020537_change_purchase_price_in_cents_to_integer.rb
+++
@@ -1,5 +0,0 @@
-class ChangePurchasePriceInCentsToInteger < ActiveRecord::Migration
-  def change
-    change_column :purchases, :price_in_cents, :integer
-  end
-end
